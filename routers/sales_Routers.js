30ea3d1710f2fa3b44b0c60da128c2b745c658cd
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-11-25 10:09:42 +0300

    get meal and its sales

--- routers/sales_Routers.js
+++ routers/sales_Routers.js
@@ -1,11 +1,12 @@
 const salesRouter = require("express").Router();
 
-const { salesForEachOrder, salesForAllOrders, salesForToday, previousdaySales, lastWeekSales, salesForSpecificTimeRange } = require('../controllers/sales_Controllers');
+const { salesForEachOrder, salesForAllOrders, salesForToday, previousdaySales, lastWeekSales, salesForSpecificTimeRange, mealSales } = require('../controllers/sales_Controllers');
 salesRouter.get("/sales/orders", salesForEachOrder);
 salesRouter.get("/sales", salesForAllOrders);
 salesRouter.get("/sales/today", salesForToday);
 salesRouter.get("/sales/previousday", previousdaySales);
 salesRouter.get("/sales/pastweek", lastWeekSales);
 salesRouter.get("/sales/timespan", salesForSpecificTimeRange);
+salesRouter.get("/sales/meals", mealSales);
 
 module.exports = { salesRouter };
