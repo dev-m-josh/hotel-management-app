9b263d6aafe6fb30403811df6501a5144a11ad6a
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-11-25 09:18:30 +0300

    sales of specific time span

--- controllers/sales_Controllers.js
+++ controllers/sales_Controllers.js
@@ -128,4 +128,80 @@ WHERE
   );
 }
 
-module.exports = { salesForEachOrder, salesForAllOrders, salesForToday, previousdaySales };
+//sales for the last 7days
+function lastWeekSales(req, res) {
+  let pool = req.pool;
+  pool.query(
+    `SELECT 
+    SUM(oi.quantity * mi.price) AS total_sales_last_7_days
+FROM 
+    order_items oi
+JOIN 
+    menu_items mi ON oi.meal_id = mi.meal_id
+JOIN 
+    orders o ON oi.order_id = o.order_id
+WHERE 
+    o.created_at >= CAST(DATEADD(DAY, -7, GETDATE()) AS DATE)  -- Last 7 days
+    AND o.created_at < CAST(GETDATE() AS DATE)`,
+    (err, result) => {
+      if (err) {
+        res.status(500).json({
+          success: false,
+          message: "Internal server error.",
+        });
+        console.log("Error occured in query", err);
+      }
+      if (result.rowsAffected[0] === 0) {
+        res.json({
+          message: "No sales made in the last week",
+        });
+      } else {
+        res.json(result.recordset);
+      }
+    }
+  );
+}
+
+//sales for specific date range
+function salesForSpecificTimeRange(req, res) {
+  let date = req.body;
+  let pool = req.pool;
+  pool.query(
+    `SELECT 
+    SUM(oi.quantity * mi.price) AS total_sales
+FROM 
+    order_items oi
+JOIN 
+    menu_items mi ON oi.meal_id = mi.meal_id
+JOIN 
+    orders o ON oi.order_id = o.order_id
+WHERE 
+    o.created_at >= '${date.start}'
+    AND o.created_at < '${date.end}'`,
+    (err, result) => {
+      if (err) {
+        res.status(500).json({
+          success: false,
+          message: "Internal server error.",
+        });
+        console.log("Error occured in query", err);
+      }
+      if (result.rowsAffected[0] === 0) {
+        res.json({
+          message: "No sales made in the last week",
+        });
+      } else {
+        res.json(result.recordset);
+      }
+    }
+  );
+}
+
+module.exports = {
+  salesForEachOrder,
+  salesForAllOrders,
+  salesForToday,
+  previousdaySales,
+  lastWeekSales,
+  salesForSpecificTimeRange,
+};
--- routers/sales_Routers.js
+++ routers/sales_Routers.js
@@ -1,9 +1,11 @@
 const salesRouter = require("express").Router();
 
-const { salesForEachOrder, salesForAllOrders, salesForToday, previousdaySales } = require('../controllers/sales_Controllers');
+const { salesForEachOrder, salesForAllOrders, salesForToday, previousdaySales, lastWeekSales, salesForSpecificTimeRange } = require('../controllers/sales_Controllers');
 salesRouter.get("/sales/orders", salesForEachOrder);
 salesRouter.get("/sales", salesForAllOrders);
 salesRouter.get("/sales/today", salesForToday);
-salesRouter.get("/sales/previous", previousdaySales)
+salesRouter.get("/sales/previousday", previousdaySales);
+salesRouter.get("/sales/pastweek", lastWeekSales);
+salesRouter.get("/sales/timespan", salesForSpecificTimeRange);
 
 module.exports = { salesRouter };
