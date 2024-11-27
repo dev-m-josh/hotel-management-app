899c29cb8a3db335cd97249a8091c34e59aa9435
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-11-27 17:25:04 +0300

    header

--- UI/hotel-management-app/src/App.js
+++ UI/hotel-management-app/src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import Header from "./Components/Header";
-import SignUp from "./Components/SignUp";
 
 function App() {
   return (
     <>
       <Header />
       <div className="App">
-        <SignUp />
       </div>
     </>
   );
