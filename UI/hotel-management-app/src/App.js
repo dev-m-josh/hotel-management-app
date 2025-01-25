d44e93a378001e7f2667297e1507ad7e2541a99e
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2025-01-25 12:51:43 +0300

    rs

--- UI/hotel-management-app/src/App.js
+++ UI/hotel-management-app/src/App.js
@@ -1,14 +1,30 @@
-import "./App.css";
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
+import Meals from "./Components/Meals";  
+import Staffs from "./Components/Staffs";
+import "./App.css";
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
+import Orders from "./Components/Orders";
 
 function App() {
   return (
     <>
       <Header />
       <div className="App">
+        <Routes>
+          <Route path="/" element={<Meals />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/staffs" element={<Staffs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
       </div>
     </>
   );
 }
 
 export default App;
+
