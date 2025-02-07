b812055a5f8230bfe1e68ccb136d2457f55ad443
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2025-02-07 17:19:52 +0300

    styles for the header

--- UI/hotel-management-app/src/Components/Header.js
+++ UI/hotel-management-app/src/Components/Header.js
@@ -1,26 +1,38 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
+import "../Styles/Header.css";
 
 function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <h1>Food</h1>
+        <h1>FoodX</h1>
       </div>
 
       <nav className="header-details">
         <div className="links">
-          <a href="/" className="nav-link active">Home</a>
-          <a href="/menu" className="nav-link">Menu</a>
-          <a href="/staffs" className="nav-link">Staffs</a>
+          <a href="/" className="nav-link active">
+            Home
+          </a>
+          <a href="/menu" className="nav-link">
+            Menu
+          </a>
+          <a href="/staffs" className="nav-link">
+            Staffs
+          </a>
+          <div className="cart">
+            <FontAwesomeIcon className="icon" icon={faShoppingBasket} />
+            <div className="cart-number">0</div>
+          </div>
         </div>
 
         <div className="cart-signup">
-          <div className="cart">
-            <FontAwesomeIcon className='icon' icon={faShoppingBasket} />
-            <div className="cart-number">0</div>
-          </div>
-          <button className="signup-btn">Sign Up</button>
+          <a href="/sign-up" className="signup-btn">
+            Login
+          </a>
+          <a href="/sign-up" className="signup-btn">
+            Sign Up
+          </a>
         </div>
       </nav>
     </header>
