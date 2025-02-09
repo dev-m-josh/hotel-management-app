3191333afaa4884c1edbed11cc396f5d2f1cfb24
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2025-02-09 18:05:30 +0300

    header css styles

--- UI/hotel-management-app/src/Components/Header.js
+++ UI/hotel-management-app/src/Components/Header.js
@@ -1,30 +1,50 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import "../Styles/Header.css";
 
 function Header() {
-  return (
-    <header className="header">
-      <div className="logo">
-        <h1>Food</h1>
-      </div>
+    // Get the current path
+    const currentPath = window.location.pathname;
 
-      <nav className="header-details">
-        <div className="links">
-          <a href="/" className="nav-link active">Home</a>
-          <a href="/menu" className="nav-link">Menu</a>
-          <a href="/staffs" className="nav-link">Staffs</a>
-        </div>
+    return (
+        <header className="header">
+            <div className="logo">
+                <h1>FoodX</h1>
+            </div>
 
-        <div className="cart-signup">
-          <div className="cart">
-            <FontAwesomeIcon className='icon' icon={faShoppingBasket} />
-            <div className="cart-number">0</div>
-          </div>
-          <button className="signup-btn">Sign Up</button>
-        </div>
-      </nav>
-    </header>
-  );
+            <nav className="header-details">
+                <div className="links">
+                    <a
+                        href="/"
+                        className={`nav-link ${currentPath === '/' ? 'active' : ''}`}
+                    >
+                        Home
+                    </a>
+                    <a
+                        href="/orders"
+                        className={`nav-link ${currentPath === '/orders' ? 'active' : ''}`}
+                    >
+                        Orders
+                    </a>
+                    <a
+                        href="/staffs"
+                        className={`nav-link ${currentPath === '/staffs' ? 'active' : ''}`}
+                    >
+                        Staffs
+                    </a>
+                    <div className="cart">
+                        <FontAwesomeIcon className='icon' icon={faShoppingBasket} />
+                        <div className="cart-number">0</div>
+                    </div>
+                </div>
+
+                <div className="cart-signup">
+                    <a href="/login" className="signup-btn">Log In</a>
+                    <a href="/signup" className="signup-btn">Sign Up</a>
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
